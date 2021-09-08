ba7b22a7026bf11cf679d164340bec3aa5965c5a
Zius-C0der <90148348+Zius-C0der@users.noreply.github.com>
2021-09-08 19:31:57 +0200

    Division/Multiplication Test

--- src/main/java/javaNotes/App.java
+++ src/main/java/javaNotes/App.java
@@ -6,29 +6,17 @@ package javaNotes;
  */
 public class App {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
 
         String myName = "Zius1";
         String hisName = "Gunslinger";
-        System.out.println(myName + "&"+ hisName);
+        System.out.println(myName + "&" + hisName);
 
         Integer myAge = 27;
         int hisAge = 30;
 
-        boolean ageComparison = myAge > hisAge;
+        boolean ageComparison = myAge < hisAge;
         System.out.println(ageComparison);
 
-        int result =  sumOperation(myAge,hisAge) + sumOperation(4,9);
-
-        System.out.println(result);
-    }
-
-    public static int sumOperation(int number1, int number2) {
-        return number1 + number2;
-    }
-
-    public static int subOperation(int number1, int number2) {
-        return number1 - number2;
     }
 }
-
